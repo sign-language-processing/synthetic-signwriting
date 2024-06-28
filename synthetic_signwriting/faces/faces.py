fixing big problem with the code and changing the normalization

synthetic_signwriting/faces/faces.py
@@ -1,6 +1,10 @@
 import math
 import pickle
+from functools import lru_cache
+from pathlib import Path
+from pose_format.utils.normalization_3d import PoseNormalizer, PoseNormalizationInfo
 import numpy as np
+import numpy.ma as ma
 from scipy.spatial.transform import Rotation
 
 
@@ -17,18 +21,10 @@ def rotate_face(pose: np.ndarray, angle: float):
     return np.dot(pose, rotation.as_matrix())
 
 
-def scale_face(pose: np.ndarray, suitable_face: np.ndarray = None, size=200):
-    point1 = pose[4]  # Nose
-    point2 = pose[6]  # Middle eyebrows
-    current_size = np.sqrt(np.power(point2 - point1, 2).sum())
-
-    if suitable_face is not None:
-        point1 = suitable_face[4]  # Nose of suitable hand
-        point2 = suitable_face[6]  # Middle eyebrows of suitable hand
-        size = np.sqrt(np.power(point2 - point1, 2).sum())
-
-    pose *= size / current_size
-    pose -= pose[4] - suitable_face[4]  # move to Nose of the suitable hand
+def reposition_face(pose: np.ndarray, suitable_face: np.ndarray):
+    pose_center_point = pose[4]  # Nose
+    suitable_face_center_point = suitable_face[4]  # Nose of suitable hand
+    pose += suitable_face_center_point - pose_center_point  # move to Nose of the suitable hand
     return pose
 
 
@@ -43,22 +39,34 @@ def normalized_face(pose: np.ndarray):
     return pose
 
 
-def prorate_face(pose: np.ndarray, suitable_face: np.ndarray):
+def proportionate_face(pose: np.ndarray, suitable_face: np.ndarray):
     assert pose.shape == (468, 3) and suitable_face.shape == (468, 3)
     assert not np.all(pose == 0) and not np.all(suitable_face == 0)
-    pose = normalized_face(pose)
 
-    # Scale pose such that BASE-M_CMC is of size of the suitable face
-    pose = scale_face(pose, suitable_face)
+    # reposition face to the center of the suitable face
+    pose = reposition_face(pose, suitable_face)
 
     return pose
 
 
-def read_pkl_file(file_path):
+def normalize_face(normalizer: PoseNormalizer, pose: np.ndarray):
+    pose = normalizer(ma.masked_array([[pose]]))[0][0]
+    return pose
+
+
+def face_normalization():
+    plane = PoseNormalizationInfo(4, 362, 133)  # Nose, Right eye inner, Left eye inner
+    line = PoseNormalizationInfo(4, 6)  # Nose, Middle eyebrows
+    return PoseNormalizer(plane, line, 45)
+
+
+@lru_cache()
+def load_faces():
+    file_path = Path(__file__).parent.parent / "data" / "faces.pkl"
     try:
         with open(file_path, "rb") as file:
             data = pickle.load(file)
         return data
-    except Exception as exp:    # pylint: disable=broad-except
+    except Exception as exp:  # pylint: disable=broad-except
         print(exp)
         return None

synthetic_signwriting/hands/hands.py
@@ -1,7 +1,18 @@
 import math
+from functools import lru_cache
+from pathlib import Path
+from pose_format.utils.normalization_3d import PoseNormalizer, PoseNormalizationInfo
 import numpy as np
-
+import random
+import numpy.ma as ma
 from scipy.spatial.transform import Rotation
+
+
+def randomize_hand_pose(pose: np.ndarray):
+    matrix_type = random.choice([0, 1])
+    shape = random.choice(range(261))
+    orientations = random.choice(range(6))
+    return pose[matrix_type][shape][orientations]
 
 
 def create_left_hand(pose: np.ndarray):
@@ -43,7 +54,14 @@ def scale_hand(pose: np.ndarray, suitable_hand: np.ndarray = None, size=200):
     return pose
 
 
-def prorate_hand(pose: np.ndarray, suitable_hand: np.ndarray, reflection=False):
+def reposition_hand(pose: np.ndarray, suitable_hand: np.ndarray):
+    pose_center_point = pose[0]  # Wrist
+    suitable_hand_center_point = suitable_hand[0]  # Wrist of suitable hand
+    pose += suitable_hand_center_point - pose_center_point  # move to Wrist of the suitable hand
+    return pose
+
+
+def proportionate_hand(pose: np.ndarray, suitable_hand: np.ndarray, reflection=False):
     assert pose.shape == (21, 3) and suitable_hand.shape == (21, 3)
     assert not np.all(pose == 0) and not np.all(suitable_hand == 0)
 
@@ -53,12 +71,24 @@ def prorate_hand(pose: np.ndarray, suitable_hand: np.ndarray, reflection=False):
         pose = create_left_hand(pose)
 
     # Scale pose such that BASE-M_CMC is of size of the suitable hand
-    pose = scale_hand(pose, suitable_hand)
+    pose = reposition_hand(pose, suitable_hand)
 
     return pose
 
 
-def read_npy_file(file_path):
+def normalize_hand(normalizer: PoseNormalizer, pose: np.ndarray):
+    return normalizer(ma.masked_array([[pose]]))[0][0]
+
+
+def hands_normalization():
+    plane = PoseNormalizationInfo(0, 17, 5)  # wrist, middle CMC, thumb CMC
+    line = PoseNormalizationInfo(0, 9)  # wrist, middle finger CBC
+    return PoseNormalizer(plane, line, 95)
+
+
+@lru_cache()
+def load_hands():
+    file_path = Path(__file__).parent.parent / "data" / "hands.npy"
     try:
         data = np.load(file_path)
         return data

synthetic_signwriting/synthetic_pose_helper.py
@@ -2,66 +2,70 @@ import random
 from pose_format import Pose
 import numpy as np
 
-from synthetic_signwriting.hands.hands import read_npy_file, prorate_hand, get_hand_rotation, rotate_hand
+from synthetic_signwriting.hands.hands import load_hands, normalize_hand, hands_normalization, create_left_hand, rotate_hand
+from synthetic_signwriting.faces.faces import normalize_face, face_normalization, proportionate_face
+
+RightHand_Range = slice(522, 543)  # 522:543 is the right hand
+LeftHand_Range = slice(501, 522)  # 501:522 is the left hand
+Head_Range = slice(33, 501)  # 33:501 is the head
 
 
-def randomize_hand_pose(pose: np.ndarray):
-    matrix_type = random.choice([0, 1])
-    shape = random.choice(range(261))
-    orientations = random.choice(range(6))
-    return pose[matrix_type][shape][orientations]
-
-
-def create_base_frame(pose: Pose, next_pose: np.ndarray = None):
+def create_base_frame(starting_pose: np.ndarray):
     open_hand = 77  # simple hand that is open
-    starting_pose = pose.body.data[0][0].copy()  # get the first frame
-    hand_data_path = 'data/hands.npy'
-    hand_data = read_npy_file(hand_data_path)
+    hand_data = load_hands()
     matrix_type = random.choice([0, 1])
+    hand_normalizer = hands_normalization()
+    face_normalizer = face_normalization()
 
     # rotate the hands to be in the right position
     basic_hand = hand_data[matrix_type][open_hand][0]
-    basic_hand = rotate_hand(basic_hand, get_hand_rotation(starting_pose) + 180)
-    basic_hand = rotate_hand(basic_hand, 90, 'x')  # rotate the hand to rest position
+    basic_hand = normalize_hand(hand_normalizer, basic_hand)
+    basic_hand = rotate_hand(basic_hand, 180, 'z')  # rotate the hand to rest position
 
-    next_right_hand = next_pose[522:543]  # get the right hand base that located (522:543)
-    right_hand = prorate_hand(basic_hand, next_right_hand)
+    right_hand = basic_hand.copy()
     right_wrist = starting_pose[16]  # 16 is the right wrist
     right_hand += right_wrist - right_hand[0]  # normalize the right hand
 
-    next_left_hand = next_pose[501:522]  # get the left hand base that located (501:522)
-    left_hand = prorate_hand(basic_hand, next_left_hand, reflection=True)
+    left_hand = create_left_hand(basic_hand)
     left_wrist = starting_pose[15]  # 15 is the left wrist
     left_hand += left_wrist - left_hand[0]  # normalize the left hand
 
+    normalized_face = normalize_face(face_normalizer, starting_pose[Head_Range].copy())     # normalize the face
+    normalized_face = proportionate_face(normalized_face, starting_pose[Head_Range])        # proportionate the face
+
     # update the right and left hand
-    starting_pose[522:543] = right_hand
-    starting_pose[501:522] = left_hand
+    starting_pose[RightHand_Range] = right_hand
+    starting_pose[LeftHand_Range] = left_hand
+    starting_pose[Head_Range] = normalized_face
 
     return starting_pose
 
 
 def pose_transition(begin_position: np.ndarray, end_position: np.ndarray, frames: int, buffer: int = 30):
     assert begin_position.shape == end_position.shape
+    begin_position = begin_position[0]
+    end_position = end_position[0]
+
     transition = np.zeros((frames + buffer, *begin_position.shape))
     step = (end_position - begin_position) / frames
     transition[0] = begin_position
+
     for i in range(1, frames):
         #   go one step forward
         transition[i] = transition[i - 1] + step
 
         #   normalize the right hand based on the right wrist
-        right_hand = transition[i][522:543]  # 522:543 is the right hand
+        right_hand = transition[i][RightHand_Range]  # retrieve the right hand
         right_wrist = transition[i][16]  # 16 is the right wrist
         right_hand += right_wrist - right_hand[0]  # normalize the right hand
 
-        left_hand = transition[i][501:522]  # 501:522 is the left hand
+        left_hand = transition[i][LeftHand_Range]  # retrieve the left hand
         left_wrist = transition[i][15]  # 15 is the left wrist
         left_hand += left_wrist - left_hand[0]  # normalize the left hand
 
         #  update the right and left hand
-        transition[i][522:543] = right_hand
-        transition[i][501:522] = left_hand
+        transition[i][RightHand_Range] = right_hand
+        transition[i][LeftHand_Range] = left_hand
         step = (end_position - transition[i]) / (frames - i)
 
     # add suspension frames

synthetic_signwriting/synthetic_signwriting.py
@@ -1,37 +1,52 @@
 import random
+from pathlib import Path
 
 import numpy as np
 from pose_format import Pose
 
-from synthetic_signwriting.hands.hands import read_npy_file, prorate_hand
-from synthetic_signwriting.faces.faces import prorate_face, read_pkl_file
-from synthetic_signwriting.synthetic_pose_helper import randomize_hand_pose, create_base_frame, pose_transition
-
+from synthetic_signwriting.hands.hands import load_hands, proportionate_hand, randomize_hand_pose, hands_normalization, normalize_hand
+from synthetic_signwriting.faces.faces import proportionate_face, load_faces, face_normalization, normalize_face
+from synthetic_signwriting.synthetic_pose_helper import create_base_frame, pose_transition
 
 class SyntheticSignWriting:
 
     def __init__(self):
-        with open('data/base_pose.pose', 'rb') as file:
-            data_buffer = file.read()
-        pose = Pose.read(data_buffer)
-        middle = int(len(pose.body.data) / 2)
+        base_pose_path = Path(__file__).parent / "data" / "base_pose.pose"
+        with open(base_pose_path, 'rb') as file:
+            pose = Pose.read(file.read())
+        middle_frame = int(len(pose.body.data) / 2)
 
-        self.hand_data = read_npy_file('data/hands.npy')
-        self.face_data = read_pkl_file('data/faces.pkl')
+        # load the hand and face data
+        self.hand_data = load_hands()
+        self.face_data = load_faces()
+
+        # initialize the pose
         self.pose = pose
-        self.static_position = [pose.body.data[middle + 2 * i][0].copy() for i in range(4)]
-        self.hands = []
-        self.faces = []
+        # initialize static position based on the base pose - will use as a base for the keyframes
+        self.static_position = [pose.body.data[middle_frame + 2 * i][0].copy() for i in range(4)]
+        self.start_position = pose.body.data[0][0].copy()     # the first frame
+        self.sequence = []
+
+        # initialize the hands and faces locations
+        self.feature_range = {}
+        offset = 0  # offset to keep track of the current location
+        for component in self.pose.header.components:
+            if component.name == "RIGHT_HAND_LANDMARKS":
+                self.feature_range["right_hand"] = slice(offset, offset + len(component.points))
+            if component.name == "LEFT_HAND_LANDMARKS":
+                self.feature_range["left_hand"] = slice(offset, offset + len(component.points))
+            if component.name == "FACE_LANDMARKS":
+                self.feature_range["face"] = slice(offset, offset + len(component.points))
+            offset += len(component.points)
 
     def add_keyframe(self, face=None, left_hand=None, right_hand=None):
         # Create and configure a new frame
-        self.hands.append((left_hand, right_hand))
-        self.faces.append(face)
+        self.sequence.append((face, left_hand, right_hand))
 
     def add_random_keyframe(self):
         # choose a random hand and face pose
-        right_hand = randomize_hand_pose(self.hand_data)  # choose a random hand pose
-        left_hand = randomize_hand_pose(self.hand_data)  # choose a random hand pose
+        right_hand = randomize_hand_pose(self.hand_data)
+        left_hand = randomize_hand_pose(self.hand_data)
 
         # choose a random face pose
         face_type = random.choice(list(self.face_data.keys()))
@@ -39,8 +54,8 @@ class SyntheticSignWriting:
         if element == "source":
             face = self.face_data[face_type][element]
         else:
-            cathartic = random.choice(list(self.face_data[face_type][element].keys()))
-            face = self.face_data[face_type][element][cathartic]
+            characteristic = random.choice(list(self.face_data[face_type][element].keys()))
+            face = self.face_data[face_type][element][characteristic]
 
         # add the keyframe
         self.add_keyframe(face=face, left_hand=left_hand, right_hand=right_hand)
@@ -50,30 +65,38 @@ class SyntheticSignWriting:
         return "..."
 
     def render_pose(self, frame_suspension=100):
+        # initialize the pose
+        _, _, points, dimensions = self.pose.body.data.shape    # get the number of points and dimensions
         self.pose.body.data = None
-        zero_frame = np.zeros((frame_suspension - 2, 1, 543, 3))  # the -2 is to remove the first and last frame
+        zero_frame = np.zeros((frame_suspension - 2, 1, points, dimensions))    # the -2 is because first and last frame
+        hands_normalizer = hands_normalization()
+        face_normalizer = face_normalization()
 
-        for face, (left_hand, right_hand) in zip(self.faces, self.hands):
+        for (face, left_hand, right_hand) in self.sequence:
             selected_frame = random.choice(self.static_position)  # choose a random base frame
-            pose_frame = self.static_position[selected_frame][0].copy()
+            pose_frame = selected_frame.copy()
 
             # hand
-            right_hand_base = pose_frame[522:543]  # get the right hand base that located (522:543)
-            right_hand = prorate_hand(right_hand, right_hand_base)
-            pose_frame[522:543] = right_hand
-            left_hand_base = pose_frame[501:522]  # get the left hand base that located (501:522)
-            left_hand = prorate_hand(left_hand, left_hand_base, reflection=True)  # reflection because it's left hand
-            pose_frame[501:522] = left_hand
+            right_hand_base = pose_frame[self.feature_range["right_hand"]]  # get the right hand
+            right_hand = normalize_hand(hands_normalizer, right_hand)  # normalize the right hand
+            right_hand = proportionate_hand(right_hand, right_hand_base)
+            pose_frame[self.feature_range["right_hand"]] = right_hand
+
+            left_hand_base = pose_frame[self.feature_range["left_hand"]]  # get the left hand
+            left_hand = normalize_hand(hands_normalizer, left_hand)
+            left_hand = proportionate_hand(left_hand, left_hand_base, reflection=True)  # reflection because it's left hand
+            pose_frame[self.feature_range["left_hand"]] = left_hand
 
             # face
-            face_base = pose_frame[33:501]  # get the face base that located (33:501)
-            face = prorate_face(face, face_base)
-            pose_frame[33:501] = face
+            face_base = pose_frame[self.feature_range["face"]]  # get the face
+            face = normalize_face(face_normalizer, face)
+            face = proportionate_face(face, face_base)
+            pose_frame[self.feature_range["face"]] = face
 
             if self.pose.body.data is None:
-                self.pose.body.data = [[create_base_frame(pose_frame)]]
-            self.pose.body.data += zero_frame.copy()
-            self.pose.body.data.append([pose_frame])
+                self.pose.body.data = np.array([[create_base_frame(self.start_position)]])
+            self.pose.body.data = np.concatenate((self.pose.body.data, zero_frame.copy()))
+            self.pose.body.data = np.concatenate((self.pose.body.data, np.array([[pose_frame]])))
         self._smooth(frame_suspension)
         return self.pose
 